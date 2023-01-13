b84774a101c24649812ab081c45b70bb469597ff
ZikaZaki <zika.github@gmail.com>
2023-01-14 02:18:51 +0300

    Add User-Food association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_222020) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_230956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_222020) do
     t.integer "qty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_222020) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
