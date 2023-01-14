393578ba423d451ec8cb2ee11564a7783049c6e5
ZikaZaki <zika.github@gmail.com>
2023-01-14 23:44:01 +0300

    Add public-recipes views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :authenticate_user!, except: %i[index show public_recipes]
 
   # GET /recipes or /recipes.json
   def index
@@ -34,6 +34,28 @@ class RecipesController < ApplicationController
     end
   end
 
+  def public_recipes
+    @public_recipes = Recipe.where(public: true)
+  end
+
+  def new_ingredient
+    @foods = Food.all.map { |food| [food.name, food.id] }
+    @recipe_id = params[:id]
+    @recipe_food = RecipeFood.new
+  end
+
+  def create_ingredient
+    @recipe_food = RecipeFood.new
+    @recipe_food.quantity = ingredient_params[:qty]
+    @recipe_food.food = Food.find(ingredient_params[:food])
+    @recipe_food.recipe = Recipe.find(params[:id])
+    if @recipe_food.save
+      redirect_to recipe_details_path(id: @recipe_food.recipe.id)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
   # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
@@ -64,8 +86,14 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+
+
   # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+
+  def ingredient_params
+    params.require(:new_recipe_food).permit(:qty, :food)
   end
 end
