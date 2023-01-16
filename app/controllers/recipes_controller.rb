2d069ef37a1d83fdfc2772694d8119a3c2c91fdb
ZikaZaki <zika.github@gmail.com>
2023-01-16 04:09:14 +0300

    Fixed Add-Ingredient bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,8 +8,15 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def public_recipes
+    @public_recipes = Recipe.where(public: true)
+  end
+
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+  end
 
   # GET /recipes/new
   def new
@@ -29,41 +36,6 @@ class RecipesController < ApplicationController
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def public_recipes
-    @public_recipes = Recipe.where(public: true)
-  end
-
-  def new_ingredient
-    @foods = Food.all.map { |food| [food.name, food.id] }
-    @recipe_id = params[:id]
-    @recipe_food = RecipeFood.new
-  end
-
-  def create_ingredient
-    @recipe_food = RecipeFood.new
-    @recipe_food.quantity = ingredient_params[:qty]
-    @recipe_food.food = Food.find(ingredient_params[:food])
-    @recipe_food.recipe = Recipe.find(params[:id])
-    if @recipe_food.save
-      redirect_to recipe_details_path(id: @recipe_food.recipe.id)
-    else
-      redirect_back(fallback_location: root_path)
-    end
-  end
-
-  # PATCH/PUT /recipes/1 or /recipes/1.json
-  def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
@@ -89,9 +61,5 @@ class RecipesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-  end
-
-  def ingredient_params
-    params.require(:new_recipe_food).permit(:qty, :food)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  resources :recipes
+  # resources :recipes
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :foods
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root "foods#index"
+  
   root to: "recipes#public_recipes"
-  get 'recipe/new_ingredient/:id', to: "recipes#new_ingredient", as: "recipe_new_ingredient"
-  post 'recipe/create_ingredient/:id', to: "recipes#create_ingredient", as: "recipe_create_ingredient"
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
+    resources :recipe_foods
+  end
   get 'recipe/public_recipes', to: "recipes#public_recipes", as: "public_recipes"
 end
